e238ef49ffea428f9e8f4f76ee3120a1f5718903
BastardFat <favl@gendalf.ru>
2017-02-16 17:54:55 +0300

    Added 2 key classes

---
+++ BastardFat.CataclysmNews.GitHubService/Client/HttpSender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace BastardFat.CataclysmNews.GitHubService.Client
+{
+    public class HttpSender
+    {
+        public string SendGet(string url)
+        {
+            HttpWebRequest http_request = WebRequest.CreateHttp(url);
+            http_request.Method = "GET";
+            http_request.UserAgent = "Test-app";
+            http_request.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(new ASCIIEncoding().GetBytes("BastardFat" + ":" + "t367830t")));
+
+            using (var response = (HttpWebResponse) http_request.GetResponse())
+            {
+                string responseText;
+                using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                    responseText = reader.ReadToEnd();
+                return responseText;
+            }
+        }
+
+    }
+}
---
+++ BastardFat.CataclysmNews.GitHubService/EventChecker.cs
@@ -0,0 +1,51 @@
+﻿using BastardFat.CataclysmNews.GitHubService.Client;
+using BastardFat.CataclysmNews.GitHubService.Serialization.ResponseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace BastardFat.CataclysmNews.GitHubService
+{
+    class EventChecker
+    {
+        public EventChecker(string apiUrl)
+        {
+            ApiUrl = apiUrl;
+            Timer t = new Timer(1000);
+            t.Elapsed += Refresh;
+            t.Start();
+        }
+
+        public event Action<Event> IssuesEvent = delegate { };
+
+        public event Action<Event> PullRequestEvent = delegate { };
+
+
+
+        private void Refresh(object sender, ElapsedEventArgs ea)
+        {
+            var json = httpSender.SendGet(ApiUrl);
+            var events = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Event>>(json)
+                        .Where(e => (e.type == "IssuesEvent" || e.type == "PullRequestEvent") && e.created_at > lastReceivedTime)
+                        .ToList();
+
+            foreach (var item in events)
+            {
+                if (item.created_at > lastReceivedTime)
+                {
+                    lastReceivedTime = item.created_at;
+                    if (item.type == "IssuesEvent") IssuesEvent.Invoke(item);
+                    if (item.type == "PullRequestEvent") PullRequestEvent.Invoke(item);
+                }
+            }
+        }
+
+        private DateTime lastReceivedTime = DateTime.UtcNow - TimeSpan.FromDays(2);
+        private string ApiUrl;
+        private HttpSender httpSender = new HttpSender();
+
+    }
+}
