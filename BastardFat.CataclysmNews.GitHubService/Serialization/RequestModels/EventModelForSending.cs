b0f62c7e677f330c393c65863dedf81f294b0432
BastardFat <favl@gendalf.ru>
2017-02-16 17:35:04 +0300

    Created model for sending to site

---
+++ BastardFat.CataclysmNews.GitHubService/Serialization/RequestModels/EventModelForSending.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BastardFat.CataclysmNews.GitHubService.Serialization.RequestModels
+{
+    public class EventModelForSending
+    {
+        public string title { get; set; }
+        public string username { get; set; }
+        public string avatar_url { get; set; }
+        public string type { get; set; }
+        public string label { get; set; }
+        public string html { get; set; }
+    }
+}
