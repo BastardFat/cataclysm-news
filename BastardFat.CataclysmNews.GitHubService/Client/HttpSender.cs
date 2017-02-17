2face7fd8f1ec34838386970559d4736899d8ac8
BastardFat <favl@gendalf.ru>
2017-02-17 12:18:13 +0300

    Removed fucking github password from damn code And added method for sending to site

--- BastardFat.CataclysmNews.GitHubService/Client/HttpSender.cs
+++ BastardFat.CataclysmNews.GitHubService/Client/HttpSender.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace BastardFat.CataclysmNews.GitHubService.Client
@@ -12,7 +13,7 @@ namespace BastardFat.CataclysmNews.GitHubService.Client
             HttpWebRequest http_request = WebRequest.CreateHttp(url);
             http_request.Method = "GET";
             http_request.UserAgent = "Test-app";
-            http_request.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(new ASCIIEncoding().GetBytes("BastardFat" + ":" + "t367830t")));
+            http_request.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(new ASCIIEncoding().GetBytes("BastardFat:" + File.ReadAllText(@"D:\private\passw.txt"))));
 
             using (var response = (HttpWebResponse) http_request.GetResponse())
             {
@@ -20,6 +21,25 @@ namespace BastardFat.CataclysmNews.GitHubService.Client
                 using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
                     responseText = reader.ReadToEnd();
                 return responseText;
+            }
+        }
+
+        static string SendViaTcp(string address, int port, string message)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            using (TcpClient client = new TcpClient(address, 80))
+            using (var stream = client.GetStream())
+            {
+                stream.Write(data, 0, data.Length);
+                data = new byte[256];
+                var respStr = String.Empty;
+                int rec;
+                do
+                {
+                    rec = stream.Read(data, 0, data.Length);
+                    respStr += Encoding.ASCII.GetString(data, 0, rec);
+                } while (rec == data.Length);
+                return respStr;
             }
         }
 
